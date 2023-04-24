ae02f6ba31545519ad54922dfc96ffc361aede5e
raqueljoseph <raquelajoseph@gmail.com>
2023-04-24 12:54:05 -0400

    update angular app

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('frontend'));
 
 app.use('/ps4', indexRouter);
 
